67f64bd4abb80ef2f2bc2fd89d0205353440d66a
Victor Veytsman <vityavv@gmail.com>
2018-01-04 13:49:11 -0500

    Add halfway css and js so that SO can look at it

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		anchors: ["home", "blog", "about", "projects", "schoolsites"],
 		menu: "#menu",
-		sectionsColor: ["#1c1c1c", "#1c1c1c", "#1c1c1c", "#1c1c1c", "#1c1c1c"]
+		sectionsColor: ["#1c1c1c", "#1c1c1c", "#1c1c1c", "#1c1c1c", "#1c1c1c"],
+		scrollOverflow: true,
 	});
 });
